d2b2264d9cd44d62e6bc4c94d7cc7a05cddfcb85
Premnath Mali <premnathmali007@gmail.com>
2025-01-04 20:01:44 +0530

    Initial commit for project

---
+++ src/main/java/com/consultancy/services/ServicesApplication.java
@@ -0,0 +1,14 @@
+package com.consultancy.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
